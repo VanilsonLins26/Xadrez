f8a973a420b18565df7f0102f78920af110eef18
Vanilson Lins <vanilson.liins@gmail.com>
2024-06-26 14:26:21 -0300

    comecando a implementar um metodo que imprime o tabuleiro

--- xadrez-console/Program.cs
+++ xadrez-console/Program.cs
@@ -1,8 +1,11 @@
 ﻿
 using tabuleiro;
+using xadrez_console;
 
 Posicao p = new Posicao(3,4);
 
 Tabuleiro tab = new Tabuleiro(8, 8);
+
+Tela.ImprimirTabuleiro(tab);
 
 Console.WriteLine(p);
---
+++ xadrez-console/Tela.cs
@@ -0,0 +1,29 @@
+﻿
+using tabuleiro;
+
+namespace xadrez_console
+{
+    internal class Tela
+    {
+        public static void ImprimirTabuleiro(Tabuleiro tab)
+        {
+            for (int i = 0; i < tab.Linhas; i++)
+            {
+                for (int j = 0; j < tab.Linhas; j++)
+                {
+                    if (tab.Peca(i, j) == null)
+                    {
+                        Console.Write("- ");
+                    }
+                    else
+                    {
+                        Console.WriteLine(tab.Peca(i, j) + " ");
+                    }
+                   
+                }
+                Console.WriteLine();
+            }
+
+        }
+    }
+}
