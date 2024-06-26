ce8b45b39178dc54ab26b3d0a6ed01cb1a4353fb
Vanilson Lins <vanilson.liins@gmail.com>
2024-06-26 14:53:22 -0300

    Comecando a colocar pecas no tabuleiro

--- xadrez-console/Program.cs
+++ xadrez-console/Program.cs
@@ -1,11 +1,16 @@
 ﻿
 using tabuleiro;
+using Xadrez;
 using xadrez_console;
 
-Posicao p = new Posicao(3,4);
+
 
 Tabuleiro tab = new Tabuleiro(8, 8);
 
+tab.ColocarPeca(new Torre(tab, Cor.Preta), new Posicao(0, 0));
+tab.ColocarPeca(new Torre(tab, Cor.Preta), new Posicao(1, 3));
+tab.ColocarPeca(new Rei(tab, Cor.Preta), new Posicao(2, 4));
+
 Tela.ImprimirTabuleiro(tab);
 
-Console.WriteLine(p);
+Console.ReadLine();
