320a21cc66f0c0c2119df7e58122d7149e4a2942
Vanilson Lins <vanilson.liins@gmail.com>
2024-07-12 13:43:14 -0300

    salvando no git

--- xadrez-console/Xadrez/Rei.cs
+++ xadrez-console/Xadrez/Rei.cs
@@ -18,5 +18,77 @@ namespace Xadrez
         {
             return "R";
         }
+
+        private bool PodeMover(Posicao pos)
+        {
+            Peca p = Tab.Peca(pos);
+            return p == null || p.Cor != Cor;
+        }
+
+        public override bool[,] MovimentosPossiveis()
+        {
+            bool[,] mat = new bool[Tab.Linhas, Tab.Colunas];
+
+            Posicao pos = new Posicao(0, 0);
+
+            pos.DefinirValores(Posicao.Linha - 1, Posicao.Coluna);
+            if(Tab.PosicaoValida(pos) && PodeMover(pos))
+            {
+                mat[pos.Linha, pos.Coluna ] = true; 
+
+            }
+            pos.DefinirValores(Posicao.Linha - 1, Posicao.Coluna+1);
+            if (Tab.PosicaoValida(pos) && PodeMover(pos))
+            {
+                mat[pos.Linha, pos.Coluna] = true;
+
+            }
+            pos.DefinirValores(Posicao.Linha, Posicao.Coluna+1);
+            if (Tab.PosicaoValida(pos) && PodeMover(pos))
+            {
+                mat[pos.Linha, pos.Coluna] = true;
+
+            }
+            pos.DefinirValores(Posicao.Linha + 1, Posicao.Coluna + 1);
+            if (Tab.PosicaoValida(pos) && PodeMover(pos))
+            {
+                mat[pos.Linha, pos.Coluna] = true;
+
+            }
+            pos.DefinirValores(Posicao.Linha - 1, Posicao.Coluna+1);
+            if (Tab.PosicaoValida(pos) && PodeMover(pos))
+            {
+                mat[pos.Linha, pos.Coluna] = true;
+
+            }
+            pos.DefinirValores(Posicao.Linha + 1, Posicao.Coluna);
+            if (Tab.PosicaoValida(pos) && PodeMover(pos))
+            {
+                mat[pos.Linha, pos.Coluna] = true;
+
+            }
+            pos.DefinirValores(Posicao.Linha + 1, Posicao.Coluna-1);
+            if (Tab.PosicaoValida(pos) && PodeMover(pos))
+            {
+                mat[pos.Linha, pos.Coluna] = true;
+
+            }
+            pos.DefinirValores(Posicao.Linha, Posicao.Coluna-1);
+            if (Tab.PosicaoValida(pos) && PodeMover(pos))
+            {
+                mat[pos.Linha, pos.Coluna] = true;
+
+            }
+            pos.DefinirValores(Posicao.Linha - 1, Posicao.Coluna-1);
+            if (Tab.PosicaoValida(pos) && PodeMover(pos))
+            {
+                mat[pos.Linha, pos.Coluna] = true;
+
+            }
+
+            return mat;
+
+
+        }
     }
 }
--- xadrez-console/tabuleiro/Posicao.cs
+++ xadrez-console/tabuleiro/Posicao.cs
@@ -13,7 +13,11 @@ namespace tabuleiro
             Coluna = coluna;
         }
 
-       
+       public void DefinirValores(int linha, int coluna)
+        {
+            Linha = linha;
+            Coluna = coluna;
+        }
 
 
         public override string ToString()
